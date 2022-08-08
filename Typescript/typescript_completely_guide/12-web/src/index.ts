e4ddf9c8f31095ab62fb1888bb9d8c5fac815672
Mercury-x <1612395322@qq.com>
2022-08-08 22:00:01 +0800

    view initial

--- Typescript/typescript_completely_guide/12-web/src/index.ts
+++ Typescript/typescript_completely_guide/12-web/src/index.ts
@@ -1,12 +1,19 @@
 import { Collections } from './models/Collections';
+import { UserForm } from './views/UserForm';
 
 // const user = User.buildUser({ id: 1 });
 // user.fetch();
 // user.on('change', () => {
 //   console.log(user);
 // });
-const collection = Collections.buildUserCollection();
-collection.on('change', () => {
-  console.log(collection);
-});
-collection.fetch();
+// const collection = Collections.buildUserCollection();
+// collection.on('change', () => {
+//   console.log(collection);
+// });
+// collection.fetch();
+
+const root = document.getElementById('root');
+if (root) {
+  const userForm = new UserForm(root);
+  userForm.render();
+}
