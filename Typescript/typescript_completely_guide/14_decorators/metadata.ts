8a26f7c0a74bffc8a838d7e2425ca315e38525b5
xueyujian <xueyj@chinatelecom.cn>
2022-08-26 16:04:59 +0800

    add metadata on property

--- Typescript/typescript_completely_guide/14_decorators/metadata.ts
+++ Typescript/typescript_completely_guide/14_decorators/metadata.ts
@@ -1,10 +1,13 @@
 import 'reflect-metadata'
 
-class plane {
-  name:string = 'boke'
+const plane = {
+  name: 'boke',
 }
 
 Reflect.defineMetadata('note', ['Hi There', 1], plane)
+Reflect.defineMetadata('noteOnColor', 'attach to property', plane, 'color')
 
 const note = Reflect.getMetadata('note', plane)
+const noteOnColor = Reflect.getMetadata('noteOnColor', plane)
+console.log(noteOnColor)
 console.log(note)
