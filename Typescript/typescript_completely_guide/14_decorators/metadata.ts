8a7a18c5ee550c193ea5960f30d052917cad24c7
xueyujian <xueyj@chinatelecom.cn>
2022-08-26 17:51:36 +0800

    how to use controller

--- Typescript/typescript_completely_guide/14_decorators/metadata.ts
+++ Typescript/typescript_completely_guide/14_decorators/metadata.ts
@@ -1,13 +1,34 @@
 import 'reflect-metadata'
 
-const plane = {
-  name: 'boke',
+@printMetadata
+class Plane {
+  color: string = 'red'
+
+  @markFunction('Hi THERE')
+  fly(): void {
+    console.log('vrrrrrrrrrrrrr')
+  }
 }
 
-Reflect.defineMetadata('note', ['Hi There', 1], plane)
-Reflect.defineMetadata('noteOnColor', 'attach to property', plane, 'color')
+// Reflect.defineMetadata('note', ['Hi There', 1], plane)
+// Reflect.defineMetadata('noteOnColor', 'attach to property', plane, 'color')
 
-const note = Reflect.getMetadata('note', plane)
-const noteOnColor = Reflect.getMetadata('noteOnColor', plane)
-console.log(noteOnColor)
-console.log(note)
+// const note = Reflect.getMetadata('note', plane)
+// const noteOnColor = Reflect.getMetadata('noteOnColor', plane)
+// console.log(noteOnColor)
+// console.log(note)
+
+function markFunction(secretInfo: string) {
+  return function (target: Plane, key: string) {
+    Reflect.defineMetadata('secret', secretInfo, target, key)
+  }
+  
+}
+// const secret = Reflect.getMetadata('secret', Plane.prototype, 'fly')
+
+function printMetadata(target: typeof Plane) {
+  for (let key in target.prototype) {
+    const secret = Reflect.getMetadata('secret', target.prototype, key)
+    console.log(secret)
+  }
+}
