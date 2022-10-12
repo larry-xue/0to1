d74bc203b8c251b4555a283b7692211e2349d0b4
Mercury-x <1612395322@qq.com>
2022-10-12 22:55:13 +0800

    环境变量

--- webpack/webpack-demo/webpack.config.js
+++ webpack/webpack-demo/webpack.config.js
@@ -7,74 +7,78 @@ const {
   WebpackManifestPlugin
 } = require('webpack-manifest-plugin')
 
-module.exports = {
-  mode: 'development',
-  entry: {
-    index: './src/index.js',
-  },
-  output: {
-    filename: '[name].[contenthash].js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true, // 清理dist文件夹
-    publicPath: '/',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Caching',
-    }), new HtmlWebpackPlugin({ // Also generate a test.html
-      filename: () => `test.html`,
-    }), new WebpackManifestPlugin({})
-  ],
-  devtool: 'inline-source-map', // 追踪代码错误
-  devServer: {
-    static: './dist', // 告知 dev serve 从什么位置查找文件
-  },
-  module: {
-    rules: [{
-      test: /\.css$/i,
-      // 模块 loader 可以链式调用。链中的每个 loader 都将对资源进行转换
-      use: ['style-loader', 'css-loader'],
-    }, {
-      test: /\.less$/i,
-      use: ['style-loader', 'css-loader', 'less-loader'],
-    }, {
-      test: /\.(png|svg|jpg|jpeg|gif)$/i,
-      type: 'asset/resource',
-    }, {
-      test: /\.(woff|woff2|eot|ttf|otf)$/i,
-      type: 'asset/resource',
-    }, {
-      test: /\.toml$/i,
-      type: 'json',
-      parser: {
-        parse: toml.parse,
-      },
-    }, {
-      test: /\.yaml$/i,
-      type: 'json',
-      parser: {
-        parse: yaml.parse,
-      },
-    }, {
-      test: /\.json5$/i,
-      type: 'json',
-      parser: {
-        parse: json5.parse,
-      },
-    }]
-  },
-  optimization: {
-    // SplitChunksPlugin 可以用于将模块分离到单独的 bundle 中
-    runtimeChunk: 'single',
-    moduleIds: 'deterministic', // 修复 vendor hash 值变化
-    splitChunks: {
-      cacheGroups: {
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name: 'vendors',
-          chunks: 'all',
+module.exports = (env) => {
+  console.log('env is: ');
+  console.log(env);
+  return {
+    mode: 'development',
+    entry: {
+      index: './src/index.js',
+    },
+    output: {
+      filename: '[name].[contenthash].js',
+      path: path.resolve(__dirname, 'dist'),
+      clean: true, // 清理dist文件夹
+      publicPath: '/',
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'Caching',
+      }), new HtmlWebpackPlugin({ // Also generate a test.html
+        filename: () => `test.html`,
+      }), new WebpackManifestPlugin({})
+    ],
+    devtool: 'inline-source-map', // 追踪代码错误
+    devServer: {
+      static: './dist', // 告知 dev serve 从什么位置查找文件
+    },
+    module: {
+      rules: [{
+        test: /\.css$/i,
+        // 模块 loader 可以链式调用。链中的每个 loader 都将对资源进行转换
+        use: ['style-loader', 'css-loader'],
+      }, {
+        test: /\.less$/i,
+        use: ['style-loader', 'css-loader', 'less-loader'],
+      }, {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      }, {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      }, {
+        test: /\.toml$/i,
+        type: 'json',
+        parser: {
+          parse: toml.parse,
         },
-      },
-    }
-  },
+      }, {
+        test: /\.yaml$/i,
+        type: 'json',
+        parser: {
+          parse: yaml.parse,
+        },
+      }, {
+        test: /\.json5$/i,
+        type: 'json',
+        parser: {
+          parse: json5.parse,
+        },
+      }]
+    },
+    optimization: {
+      // SplitChunksPlugin 可以用于将模块分离到单独的 bundle 中
+      runtimeChunk: 'single',
+      moduleIds: 'deterministic', // 修复 vendor hash 值变化
+      splitChunks: {
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'vendors',
+            chunks: 'all',
+          },
+        },
+      }
+    },
+  }
 }
